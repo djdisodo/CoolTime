6ff544e7a87cf2b87e0711404395558762e92888
djdisodo <jisoohero30@gmail.com>
2017-05-08 18:53:55 +0900

    Create Cooltime.php

---
+++ Cooltime.php
@@ -0,0 +1,10 @@
+$playername = strtolower( $playername );
+$playername = base_convert( $playername, 35, 10);
+$shmid = shmop_open($playername, 'c', 0755, 1024);
+if ( shmop_read( $shmid, 0, 11 ) <= time() ) {
+ onUse();
+ shmop_write($shmid, time() + $cooltime, 0);
+} else {
+ $cl = shmop_read( $shmid, 0, 11 ) - time();
+ onCooltime($cl);
+}
