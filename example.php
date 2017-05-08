5605e105fef7b54ae1b5661a4d9e70a3731ff541
djdisodo <jisoohero30@gmail.com>
2017-05-08 18:59:58 +0900

    Update example.php

--- example.php
+++ example.php
@@ -1,9 +1,11 @@
+<?php
 function onUse() {
   echo '기술사용';
 }
-function onCooltime() {
+function onCooltime($cl) {
   echo '쿨타임'.$cl.'초가 남았습니다';
 }
 $playername = 'djdisodo';
 $cooltime = 60;
 include_once Cooltime.php;
+?>
